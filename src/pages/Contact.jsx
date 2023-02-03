a5ef8525365097d74cb381562327be93ee8676d1
vincevise <vincentvuram204@gmail.com>
2023-02-04 02:42:25 +0530

    responsive design

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,7 +3,7 @@ import {MdOutlineMail} from 'react-icons/md'
 
 const Contact = () => {
   return (
-    <div className="flex sm:flex-col lg:flex-row gap-6 w-full min-h-screen pt-28 px-20 pb-10">
+    <div className="flex flex-col lg:flex-row gap-6 w-full min-h-screen pt-28 px-20 pb-10">
       <div>
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3720.0661648518444!2d72.78960170000002!3d21.189530400000006!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be04d045a65504f%3A0x5715d1d45ec8e423!2sPRAYOSHA%20ARCHITECTS!5e0!3m2!1sen!2sin!4v1674387343936!5m2!1sen!2sin"
